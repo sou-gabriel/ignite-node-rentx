0958dbaf625580030133e72e03c0de621f2f0d1b
Gabriel Ramos <dev.gabrielramos@gmail.com>
2022-07-28 23:26:24 -0300

    feat: configure ts-node-dev

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,11 @@
-import express from "express";
+import express from 'express'
+
+const app = express()
+
+app.get('/', (request, response) => {
+  return response.json('Hello World')
+})
+
+app.listen(3333, () => {
+  console.log('Server is running! 🏃')
+})
