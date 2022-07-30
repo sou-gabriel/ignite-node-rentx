6d3291cdb63b7fc30d4349192f576f07ee61a6fc
Gabriel Ramos <dev.gabrielramos@gmail.com>
2022-07-29 23:19:13 -0300

    feat: creating model to category

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,18 +1,15 @@
 import { Router } from 'express'
-import { v4 as uuidv4 } from 'uuid'
+
+import { Category } from '../model/Category'
 
 export const categoriesRoutes = Router()
 
-const categories = []
+const categories: Category[] = []
 
 categoriesRoutes.post('/', (request, response) => {
   const { name, description } = request.body
 
-  const category = {
-    id: uuidv4(),
-    name,
-    description
-  }
+  const category = new Category(name, description)
 
   categories.push(category)
 
