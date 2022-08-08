4a4e52869a8019959ad293c7d4590060c499d8b2
Gabriel Ramos <dev.gabrielramos@gmail.com>
2022-08-08 20:43:14 -0300

    chore: configuring the swagger

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import express from 'express'
+import swaggerUi from 'swagger-ui-express'
 
 import { router } from './routes'
+import swaggerDocument from './swagger.json'
 
 const app = express()
 
 app.use(express.json())
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.use(router)
 
