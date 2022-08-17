f15446830d75f6428e986ecd55ec8cd9a42efb3c
Gabriel Ramos <dev.gabrielramos@gmail.com>
2022-08-16 21:08:36 -0300

    chore: create user migration

---
+++ src/modules/accounts/entities/User.ts
@@ -0,0 +1,35 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm'
+import { v4 as uuidV4 } from 'uuid'
+
+@Entity()
+export class User {
+  @PrimaryColumn()
+    id?: string
+
+  @Column()
+    name!: string
+
+  @Column()
+    username!: string
+
+  @Column()
+    email!: string
+
+  @Column()
+    password!: string
+
+  @Column()
+    driver_license!: string
+
+  @Column()
+    is_admin!: boolean
+
+  @CreateDateColumn()
+    created_at!: Date
+
+  constructor () {
+    if (!this.id) {
+      this.id = uuidV4()
+    }
+  }
+}
