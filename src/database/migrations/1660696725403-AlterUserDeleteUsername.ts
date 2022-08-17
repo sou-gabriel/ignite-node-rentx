ee90200bd04d6bbdf248184d16456f22a112dd61
Gabriel Ramos <dev.gabrielramos@gmail.com>
2022-08-16 21:41:36 -0300

    chore: drop username column into users table

---
+++ src/database/migrations/1660696725403-AlterUserDeleteUsername.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner, TableColumn } from 'typeorm'
+
+export class AlterUserDeleteUsername1660696725403 implements MigrationInterface {
+  public async up (queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropColumn('users', 'username')
+  }
+
+  public async down (queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn('users', new TableColumn({
+      name: 'username',
+      type: 'varchar'
+    }))
+  }
+}
--- src/modules/accounts/repositories/implementations/UsersRepository.ts
+++ src/modules/accounts/repositories/implementations/UsersRepository.ts
@@ -11,10 +11,9 @@ export class UsersRepository implements IUsersRepository {
     this.repository = getRepository(User)
   }
 
-  async create ({ name, username, email, driver_license, password }: ICreateUserDTO): Promise<void> {
+  async create ({ name, email, driver_license, password }: ICreateUserDTO): Promise<void> {
     const user = this.repository.create({
       name,
-      username,
       email,
       driver_license,
       password
